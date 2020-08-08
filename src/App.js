fa0f60b915a30083a695b3e124467da3e957c9b2
Hamdi-Limam <limamhamdi13@gmail.com>
2020-08-08 23:51:44 +0100

    #Finished Class component demo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./App.css";
 import PostForm from "./components/PostForm";
-import Posts from "./components/Posts";
+//import Posts from "./components/Posts";
 import store from "./store";
 import { Provider } from "react-redux";
-//import PostsClass from "./components/PostsClass";
+import PostsClass from "./components/PostsClass";
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
         <PostForm />
-        {/* <PostsClass />  {/* Calling Dispatch with Class Component */}
-        <Posts /> {/* Calling Dispatch with functional Component */}
+        <PostsClass /> {/* Calling Dispatch with Class Component */}
+        {/* <Posts /> */} {/* Calling Dispatch with functional Component */}
       </Provider>
     </div>
   );
